de1995ed00f6749f9de5968245e64523ee2dd61f
Camiloarceg <camiloarceg@gmail.com>
2022-06-03 09:06:35 -0500

    template list to order html response

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,8 +33,21 @@ $(document).ready(() => {
 
             for(let i= 0; i < data.length; i++) {
                 place=data[i];
-                template = '<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>'
-                $('section.places').append(template)
+                template = ['<article>',
+                '<div class="title_box">',
+                '<h2>' + place.name + '</h2>',
+                '<div class="price_by_night">$' + place.price_by_night + '</div>',
+                '</div>',
+                '<div class="information">',
+                '<div class="max_guest">' + place.max_guest + 'Guest(s)</div>',
+                '<div class="number_rooms">' + place.number_rooms + 'Bedroom(s)</div>',
+                '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom(s)</div>',
+                '</div>',
+                '<div class="description">',
+                place.description,
+                '</div>',
+                '</article>'];
+                $('section.places').append(template.join(''))
             }
         }
     });
