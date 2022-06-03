f4527ea1735fa9c8485d189690e1dc9b74a7fe79
diegojojoayandun <3922@holbertonschool.com>
2022-06-02 22:20:35 -0500

    Add task from t4 branch , working on js script

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -8,12 +8,8 @@ $(document).ready(() => {
         }
         $("div.amenities > h4").text(Object.values(dict).join(', '))
     });
-
     const uri = "http://localhost:5001/api/v1/status/"
-
-
-    $.get(uri, (request) =>
-{
+    $.get(uri, (request) =>{
     const selector = $('#api_status')
 
     if (request.status === "OK") {
@@ -23,5 +19,23 @@ $(document).ready(() => {
         selector.css("background-color", "#cccccc");
         selector.removeClass("available");
     }
+    });
 });
-});
+
+const url = "http://localhost:5001/api/v1/places_search";
+const dict_data = {};
+
+$.ajax({
+    type: "POST",
+    url: url,
+    data: JSON.stringify(dict_data),
+    contentType: 'application/json',
+    success: (data) => {
+        alert(data)
+        alert(data[0].name)
+        alert(data[0].id)
+        alert(data[1].name)
+        alert(data[1].id)
+    },
+    dataType: 'json'
+  });
