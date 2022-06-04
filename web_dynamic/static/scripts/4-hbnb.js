5907dc18546fac8100d5c1ccaa3bf45745f8ef12
diegojojoayandun <3922@holbertonschool.com>
2022-06-03 21:08:42 -0500

    Clean code, aply semistandar, only a function to fill places

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -23,67 +23,46 @@ $(document).ready(() => {
 
   const api = 'http://localhost:5001/api/v1/places_search';
 
+  function fillPlaceArticle (data) {
+    for (let i = 0; i < data.length; i++) {
+      const place = data[i];
+
+      const template = ['<article>',
+        '<div class="title_box">',
+        '<h2>' + place.name + '</h2>',
+        '<div class="price_by_night">$' + place.price_by_night + '</div>',
+        '</div>',
+        '<div class="information">',
+        '<div class="max_guest">' + place.max_guest + 'Guest(s)</div>',
+        '<div class="number_rooms">' + place.number_rooms + 'Bedroom(s)</div>',
+        '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom(s)</div>',
+        '</div>',
+        '<div class="user"><b>Owner: </b>' + place.Owner + '</div>',
+        '<div class="description">',
+        place.description,
+        '</div>',
+        '</article>'];
+      $('section.places').append(template.join(''));
+    }
+  }
+
   $.ajax({
     type: 'POST',
     url: api,
     data: JSON.stringify({}),
     contentType: 'application/json',
-    success: (data) => {
-      for (let i = 0; i < data.length; i++) {
-        const place = data[i];
-
-        const template = ['<article>',
-          '<div class="title_box">',
-          '<h2>' + place.name + '</h2>',
-          '<div class="price_by_night">$' + place.price_by_night + '</div>',
-          '</div>',
-          '<div class="information">',
-          '<div class="max_guest">' + place.max_guest + 'Guest(s)</div>',
-          '<div class="number_rooms">' + place.number_rooms + 'Bedroom(s)</div>',
-          '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom(s)</div>',
-          '</div>',
-          '<div class="user"><b>Owner: </b>' + place.Owner + '</div>',
-          '<div class="description">',
-          place.description,
-          '</div>',
-          '</article>'];
-        $('section.places').append(template.join(''));
-      }
-    }
-
+    success: (data) => fillPlaceArticle(data)
   });
 
   $('button').click(function () {
-    // $('SECTION.places').empty()
     $('.places article').remove();
     $.ajax({
       type: 'POST',
-      url: 'http://localhost:5001/api/v1/places_search',
+      url: api,
       data: JSON.stringify({ amenities: dict }),
       dataType: 'json',
       contentType: 'application/json',
-      success: (data) => {
-        for (let i = 0; i < data.length; i++) {
-          const place = data[i];
-          const template = ['<article>',
-            '<div class="title_box">',
-            '<h2>' + place.name + '</h2>',
-            '<div class="price_by_night">$' + place.price_by_night + '</div>',
-            '</div>',
-            '<div class="information">',
-            '<div class="max_guest">' + place.max_guest + 'Guest(s)</div>',
-            '<div class="number_rooms">' + place.number_rooms + 'Bedroom(s)</div>',
-            '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom(s)</div>',
-            '</div>',
-            '<div class="user"><b>Owner: </b>' + place.Owner + '</div>',
-            '<div class="description">',
-            place.description,
-            '</div>',
-            '</article>'];
-          $('section.places').append(template.join(''));
-        }
-      }
-
+      success: (data) => fillPlaceArticle(data)
     });
   });
 });
